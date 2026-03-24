e8d97b5f89fbb12d9fd98e127c5795961132ae6c
David Asencios <asencios_52@hotmail.com>
2026-03-24 18:44:15 -0500

    Apuntar API a Render en produccion

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://easypark-backend-n1k1.onrender.com/api",
 });
 export default api;
