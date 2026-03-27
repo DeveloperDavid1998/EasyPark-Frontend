6ea68c17681dc90ba0ef24b1964eeea3edec8ec6
David Asencios <asencios_52@hotmail.com>
2026-03-27 18:02:00 -0500

    Actualizar URL backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const api = axios.create({
-  baseURL: "https://easypark-backend-n1k1.onrender.com/api",
+  baseURL: "https://easypark-backend-u6wi.onrender.com/api",
 });
 export default api;
